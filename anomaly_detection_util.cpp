b5b352aa64265e171eb6f1de75bb778a6ec1dbd9
Aviv Harel <Avivhar13@gmail.com>
2021-10-13 20:49:50 +0300

    func1

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -2,4 +2,3 @@
 // Created by Lenovo on 13/10/2021.
 //
 #include "anomaly_detection_util.h";
-
--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+
+int func1(int x){
+    return x+1;
+}
+
 int main() {
     std::cout << "hey Aviv " << std::endl;
+    std:: cout << func1(1) << std::endl;
     return 0;
 }
+
+
 int func2(int y){
     return 5;
 }

e350223702cbb3ebd0a5520731e167e1b601b781
Aviv Harel <Avivhar13@gmail.com>
2021-10-13 20:28:11 +0300

    a new files added

---
+++ anomaly_detection_util.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lenovo on 13/10/2021.
+//
+#include "anomaly_detection_util.h";
+
--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 
 int main() {
     std::cout << "Hello, Naor!" << std::endl;
+    for(auto i =0; i<5; i++){
+        std::cout << "Aviv" << std::endl;
+    }
     return 0;
 }
